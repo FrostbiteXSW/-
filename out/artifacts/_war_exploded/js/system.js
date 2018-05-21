6b559067f70b4189ba7cbb281e7099e04a622dee
熊某人 <840646587@qq.com>
2018-05-21 20:10:48 +0800

    删除冗余数据

--- out/artifacts/_war_exploded/js/system.js
+++
@@ -1,9 +0,0 @@
-function exitSystem() {
-    localStorage.clear();
-    sessionStorage.clear();
-    window.location.href="index.jsp";
-}
-function selectTable() {
-    var form=document.getElementById("selectTable");
-    form.submit();
-}
